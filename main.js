5eba6e5709e4c73455a4a0cd20c1f516a16a4c12
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-27 18:29:50 -0500

    added loop for all creeps

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-module.exports.loop = function () {
+module.exports.loop = function() {
 
-  var creep = Game.creeps.Charlie;
+  for (let name in Game.creeps) {
+    var creep = Game.creeps.Charlie;
 
-  if ( creep.memory.working == true && creep.carry.energy == 0 ) {
-    creep.memory.working = false;
-  } else ( creep.memory.working == false && creep.carryt.energy == creep.carryCapacity ) {
-
-  }
-
-  if ( creep.memory.working == true ) {
-    if ( creep.transfer(Game.spawns.HomeVB, RESOURCE_ENERGY ) == ERR_NOT_IN_RANGE ) {
-      creep.moveTo(Game.spawns.HomeVB);
+    if (creep.memory.working == true && creep.carry.energy == 0) {
+      creep.memory.working = false;
+    } else if (
+      creep.memory.working == false &&
+      creep.carry.energy == creep.carryCapacity
+    ) {
+      creep.memory.working = true;
     }
-  } else {
-    var source = creep.pos.findClosestByPath( FIND_SOURCES );
-    if ( creep.harvest( source ) == ERR_NOT_IN_RANGE ) {
-      creep.moveTo(source)
+
+    if (creep.memory.working == true) {
+      if (
+        creep.transfer(Game.spawns.HomeVB, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE
+      ) {
+        creep.moveTo(Game.spawns.HomeVB);
+      }
+    } else {
+      var source = creep.pos.findClosestByPath(FIND_SOURCES);
+      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
+      }
     }
   }
-
-}
   
+};
