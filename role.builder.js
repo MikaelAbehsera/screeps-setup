ee4e563aa0fef39296c5ca3e1eb478000830a2b8
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-27 20:40:12 -0500

    added builder code

--- role.builder.js
+++ role.builder.js
@@ -1,12 +1,28 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.builder');
- * mod.thing == 'a thing'; // true
- */
-
 module.exports = {
+  run: function (creep) {
 
+    if (creep.memory.working == true && creep.carry.energy == 0) {
+      creep.memory.working = false;
+    } else if (
+      creep.memory.working == false &&
+      creep.carry.energy == creep.carryCapacity
+    ) {
+      creep.memory.working = true;
+    }
+
+    if (creep.memory.working == true) {
+      var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+      if (constructionSite != undefined) {
+        if (creep.build(constructionSite) === ERR_NOT_IN_RANGE) {
+          moveTo(constructionSite);
+        }
+      }
+    } else {
+      var source = creep.pos.findClosestByPath(FIND_SOURCES);
+      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
+      }
+    }
+
+  }
 };
