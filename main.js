d70993066bf4d01b316008293bfb25396e0e2c93
Mikael Abehsera <mikael.abehsera@gmail.com>
2018-12-27 19:48:15 -0500

    broke harvester code into a module, added role code in main

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var roleHarvester = require("role.harvester");
+var roleUpgrader = require("role.upgrader");
 
 module.exports.loop = function() {
 
@@ -6,7 +7,11 @@ module.exports.loop = function() {
 
     var creep = Game.creeps[name];
 
-   
-  }
+    if ( creep.memory.role === "harvester" ) {
+      roleHarvester.run(creep);
+    } else if ( creep.memory.role === "upgrader" ) {
+      roleUpgrader.run(creep);
+    }
 
+  }
 };
